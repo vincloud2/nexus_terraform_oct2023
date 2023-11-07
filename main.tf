ce387893a3ee42c3c15bfe623cf194ad8abb1411
vincloud2 <128999303+vincloud2@users.noreply.github.com>
2023-11-07 21:20:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "ec2_instance" {
 
   name = "Nexus_Artifactory_Terraform"
 
-  instance_type          = "t2.small"
+  instance_type          = "t2.medium"
   ami                    = "ami-05caa5aa0186b660f"
   key_name               = "vinod_aug2023"
   vpc_security_group_ids = ["sg-076bbf8a4ce79b6aa"]
